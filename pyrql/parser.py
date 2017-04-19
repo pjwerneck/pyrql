73f5f563754aef3fbf856a4aa08f0cd72d4dc1fc
Pedro Werneck <pjwerneck@gmail.com>
2017-04-18 23:02:40 -0300

    Pyparsing port following RFC ABNF

--- pyrql/parser.py
+++ pyrql/parser.py
@@ -2,21 +2,26 @@
 
 import pyparsing as pp
 from pyparsing import pyparsing_common as common
+from six.moves import urllib
+
+# converters:
+# number
+# epoch
+# isodate
+# date
+# boolean
+# string
+# re
+# RE
+# glob
 
 
-def make_keyword(kwd_str, kwd_value):
-    return pp.Keyword(kwd_str).setParseAction(pp.replaceWith(kwd_value))
+# autoconvert:
+# numbers
+# booleans
+# null
 
 
-def _or_call(expr, loc, toks):
-    return {'name': 'or', 'args': [toks[0], toks[1]]}
-
-
-def _and_call(expr, loc, toks):
-    if len(toks) == 1:
-        return toks[0]
-    else:
-        return {'name': 'and', 'args': [toks[0], toks[1]]}
 
 
 def _eq_call(expr, loc, toks):
@@ -50,83 +55,166 @@ def _calls(expr, loc, toks):
         return {'name': 'and', 'args': calls}
 
 
+def _unquote(expr, loc, toks):
+    return urllib.parse.unquote(toks[0])
+
+
+def _typed_value(expr, loc, toks):
+    typ_, value = toks
+
+    if typ_ == 'number':
+        value = common.number.parseString(value)[0]
+
+    elif typ_ == 'date':
+        value = common.convertToDate(value)
+
+    return value
+
+
+def _call_operator(expr, loc, toks):
+    return toks.asDict()
+
+
+def _comparison(expr, loc, toks):
+    if len(toks) == 2:
+        return {'name': 'eq', 'args': toks.asList()}
+
+    else:
+        op = toks.pop(1)
+        return {'name': op, 'args': toks.asList()}
+
+
+def _or(expr, loc, toks):
+    if len(toks) == 1:
+        return toks[0]
+    else:
+        return {'name': 'or', 'args': toks.asList()}
+
+
+def _and(expr, loc, toks):
+    if len(toks) == 1:
+        return toks[0]
+    else:
+        return {'name': 'and', 'args': toks.asList()}
+
+
+def _query(expr, loc, toks):
+    if len(toks) == 1:
+        return toks[0]
+    else:
+        return {'name': 'and', 'args': toks.asList()}
+
+# query = and
+
+# and = operator *( "&" operator )
+# operator = comparison / call-operator / group
+# call-operator = name "(" [ argument *( "," argument ) ] ")"
+# argument = call-operator / value
+# value = *nchar / typed-value / array
+# typed-value = 1*nchar ":" *nchar
+# array = "(" [ value *( "," value ) ] ")"
+# name = *nchar
+
+# comparison = name ( "=" [ name "=" ] ) value
+# group = "(" ( and / or ) ")"
+# or = operator *( "|" operator )
+
+# nchar = unreserved / pct-encoded / "*" / "+"
+# pct-encoded   = "%" HEXDIG HEXDIG
+# unreserved    = ALPHA / DIGIT / "-" / "." / "_" / "~"
+
+def make_keyword(kwd_str, kwd_value):
+    return pp.Keyword(kwd_str).setParseAction(pp.replaceWith(kwd_value))
+
+
 TRUE = make_keyword('true', True)
 FALSE = make_keyword('false', False)
 NULL = make_keyword('null', None)
 
-# functions that require specific grammar and can't use generic SIMPLE_CALL
-SORT = make_keyword('sort', None).suppress()
+
+# let's treat sort as a keyword to better handle the +- prefix syntax
+SORT = pp.Keyword('sort').suppress()
+
+# keywords for typed values
+K_NUMBER = pp.Keyword('number').suppress()
+K_STRING = pp.Keyword('string').suppress()
+K_DATE = pp.Keyword('date').suppress()
+K_DATETIME = pp.Keyword('datetime').suppress()
+K_BOOL = pp.Keyword('boolean').suppress()
 
 # grammar
 PLUS = pp.Literal('+')
 MINUS = pp.Literal('-')
 EQUALS = pp.Literal('=').suppress()
-AND = pp.Literal('&').suppress()
-OR = pp.Literal('|').suppress()
-DOT = pp.Literal('.')
 LPAR = pp.Literal('(').suppress()
 RPAR = pp.Literal(')').suppress()
-COMMA = pp.Literal(',')
 COLON = pp.Literal(':').suppress()
 
-STRING = pp.Word(pp.alphanums + '_*:+-')
-NUMBER = common.number
+UNRESERVED = pp.Word(pp.alphanums + '-:._~', exact=1)
+PCT_ENCODED = pp.Combine(pp.Literal('%') + pp.Word(pp.hexnums, exact=2)).setParseAction(_unquote)
+NCHAR = (UNRESERVED | PCT_ENCODED | '*' | '+')
 
-IDENT = common.identifier.setResultsName('name')
+NCHARS = pp.Combine(pp.OneOrMore(NCHAR))
 
-CONV_STRING = (pp.Literal('string').suppress() + COLON + STRING)
-CONV_NUMBER = (pp.Literal('number').suppress() + COLON + NUMBER)
-CONV_DATE = (pp.Literal('date').suppress() + COLON + common.iso8601_date)\
-    .setParseAction(common.convertToDate())
-# add boolean, epoch and date
+NAME = NCHARS
 
-CONV = (CONV_DATE | CONV_STRING | CONV_NUMBER)
+TYPED_STRING = (K_STRING + COLON + NCHARS)
+TYPED_NUMBER = (K_NUMBER + COLON + common.number)
+TYPED_DATE = (K_DATE + COLON + common.iso8601_date).setParseAction(common.convertToDate())
+TYPED_DATETIME = (K_DATETIME + COLON + common.iso8601_datetime).setParseAction(
+    common.convertToDatetime(fmt='%Y-%m-%dT%H:%M:%S'))
+TYPED_BOOL = (K_BOOL + COLON + (TRUE | FALSE))
 
-VALUE = (CONV | NUMBER | TRUE | FALSE | NULL | STRING)
+TYPED_VALUE = (
+    TYPED_DATETIME |
+    TYPED_DATE |
+    TYPED_NUMBER |
+    TYPED_BOOL |
+    TYPED_STRING)
 
-ARRAY = (LPAR + pp.delimitedList(VALUE) + RPAR).setParseAction(_array) | VALUE
+ARRAY = pp.Forward()
 
-EQ_EXPR = (IDENT + EQUALS + ARRAY).setParseAction(_eq_call)
-FIQL_EXPR = (IDENT + EQUALS + IDENT + EQUALS + ARRAY).setParseAction(_fiql_call)
+VALUE = TYPED_VALUE | ARRAY | NULL | NCHARS
 
-CALL = pp.Forward()
+ARRAY <<= (LPAR + pp.delimitedList(VALUE) + RPAR).setParseAction(_array)
 
-ARG = pp.Forward()
+CALL_OPERATOR = pp.Forward()
+
+ARGUMENT = CALL_OPERATOR | VALUE
+
+SORT_ARG = ((MINUS | PLUS) + VALUE).setParseAction(lambda e, l, t: tuple(t))
+SORT_ARGARRAY = pp.delimitedList(SORT_ARG).setResultsName('args')
+SORT_CALL = (SORT + LPAR + SORT_ARGARRAY + RPAR).setParseAction(_sort_call)
+
+FUNC_CALL = (NAME.setResultsName('name') + LPAR +
+             pp.Group(pp.Optional(pp.delimitedList(ARGUMENT)))
+             .setResultsName('args') + RPAR).setParseAction(_call_operator)
+
+CALL_OPERATOR <<= (SORT_CALL | FUNC_CALL)
+
+COMPARISON = (NAME + EQUALS + pp.Optional(NAME + EQUALS) + VALUE).setParseAction(_comparison)
+
+OPERATOR = pp.Forward()
+
+OR = pp.delimitedList(OPERATOR, delim=pp.Literal("|")).setParseAction(_or)
+AND = pp.delimitedList(OPERATOR, delim=pp.Literal("&")).setParseAction(_and)
 
 
-OR_EXPR = (ARG + OR + ARG).setParseAction(_or_call)
-AND_EXPR = (ARG + AND + ARG).setParseAction(_and_call)
+def _group(expr, loc, toks):
+    return toks[0]
 
-ARGLIST = pp.delimitedList(ARG).setResultsName('args')
 
-ARGARRAY = LPAR + ARGLIST + RPAR
+GROUP = (LPAR + (OR | AND) + RPAR).setParseAction(_group)
 
-ARG <<= (CALL | ARRAY | ARGARRAY)
+OPERATOR <<= (GROUP | COMPARISON | CALL_OPERATOR)
 
-SIMPLE_CALL = (IDENT + LPAR + pp.Optional(ARGLIST) + RPAR).setParseAction(_simple_call)
-
-SORT_ARG = ((MINUS | PLUS) + ARRAY).setParseAction(lambda e, l, t: tuple(t))
-SORT_ARGLIST = pp.delimitedList(SORT_ARG).setResultsName('args')
-SORT_CALL = (SORT + LPAR + SORT_ARGLIST + RPAR).setParseAction(_sort_call)
-
-CALL <<= (SORT_CALL | SIMPLE_CALL | FIQL_EXPR | EQ_EXPR)
-
-AND_CALL = (CALL + AND + CALL).setParseAction(_and_call)
-OR_CALL = (CALL + OR + CALL).setParseAction(_or_call)
-
-OPCALL = (AND_CALL | OR_CALL | CALL)
-
-OPCALLS = (pp.Optional(LPAR) + pp.delimitedList(OPCALL) + pp.Optional(RPAR)).setParseAction(_calls)
-
-CALLS = (pp.Optional(LPAR) + pp.delimitedList(OPCALLS) + pp.Optional(RPAR)).setParseAction(_calls)
-
-TOPLEVEL = CALLS
+QUERY = pp.delimitedList(AND).setParseAction(_and)
 
 
 class Parser:
 
     def parse(self, expr):
-        result = TOPLEVEL.parseString(expr)
+        result = QUERY.parseString(expr)
 
         return result[0]
 
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -15,193 +15,208 @@ CMP_OPS = ['eq', 'lt', 'le', 'gt', 'ge', 'ne']
 
 class TestTokens:
 
-    @pytest.mark.parametrize('expr', ['1', '2', '3'])
-    def test_integer(self, expr):
-        pd = pm.NUMBER.parseString(expr)
-        assert pd[0] == int(expr)
+    @pytest.mark.parametrize('exre', [('%20', ' ')])
+    def test_PCT_ENCODED(self, exre):
+        expr, rep = exre
+        pd = pm.PCT_ENCODED.parseString(expr)
+        assert pd[0] == rep
 
-    @pytest.mark.parametrize('expr', ['1.4', '2.1', '3.14', '4'])
-    def test_float(self, expr):
-        pd = pm.NUMBER.parseString(expr)
-        assert pd[0] == float(expr)
+    @pytest.mark.parametrize('exre', [('%20', ' '), ('a', 'a'), ('1', '1')])
+    def test_NCHAR(self, exre):
+        expr, rep = exre
+        pd = pm.NCHAR.parseString(expr)
+        assert pd[0] == rep
 
-    @pytest.mark.parametrize('expr', ['abc', 'DEF', 'F00'])
-    def test_string(self, expr):
-        pd = pm.STRING.parseString(expr)
-        assert pd[0] == expr
-
-    @pytest.mark.parametrize('pair', [('string:3', '3'),
-                                      ('number:3', 3),
-                                      ('date:2017-01-01', datetime.date(2017, 1, 1)),
-                                      ])
-    def test_conv(self, pair):
-        expr, expected = pair
-
-        pd = pm.CONV.parseString(expr)
-        assert pd[0] == expected
-
-    @pytest.mark.parametrize('expr', ['eq', 'sort', 'contains'])
-    def test_ident(self, expr):
-        pd = pm.IDENT.parseString(expr)
-        assert pd[0] == expr
+    @pytest.mark.parametrize('exre', [('a%20b', 'a b'), ('abc', 'abc'), ('123', '123')])
+    def test_NAME(self, exre):
+        expr, rep = exre
+        pd = pm.NAME.parseString(expr)
+        assert pd[0] == rep
 
     @pytest.mark.parametrize(
-        'pair',
-        [('1', 1), ('3.14', 3.14), ('lero', 'lero'),
-         ('true', True), ('false', False), ('null', None),
-         ('string:123', '123'), ('number:123', 123),
+        'exre',
+        [('string:3', '3'),
+         ('number:3', 3),
          ('date:2017-01-01', datetime.date(2017, 1, 1)),
-         ('(1,2,3,4)', (1, 2, 3, 4)),
+         ('datetime:2017-01-01T22:04:23', datetime.datetime(2017, 1, 1, 22, 4, 23)),
+         ('boolean:true', True),
+         ('boolean:false', False),
          ])
-    def test_value(self, pair):
-        expr, expected = pair
+    def test_TYPED_VALUE(self, exre):
+        expr, rep = exre
+
+        pd = pm.TYPED_VALUE.parseString(expr)
+        assert pd[0] == rep
+
+    @pytest.mark.parametrize(
+        'exre',
+        [('123', '123'),
+         ('number:123', 123),
+         ('date:2017-01-01', datetime.date(2017, 1, 1)),
+         ])
+    def test_VALUE(self, exre):
+        expr, rep = exre
+
+        pd = pm.VALUE.parseString(expr)
+        assert pd[0] == rep
+
+    @pytest.mark.parametrize(
+        'exre',
+        [('(123,number:123,date:2017-01-01)', ('123', 123, datetime.date(2017, 1, 1))),
+         ('(1,(2, 3, (4, 5)))', ('1', ('2', '3', ('4', '5')))),
+         ])
+    def test_ARRAY(self, exre):
+        expr, rep = exre
 
         pd = pm.ARRAY.parseString(expr)
-        assert pd[0] == expected
-
-    @pytest.mark.parametrize(
-        'pair',
-        [('1', 1), ('3.14', 3.14), ('lero', 'lero'), ('true', True),
-         ('false', False), ('null', None), ('(1, 2, 3)', (1, 2, 3))])
-    def test_arg(self, pair):
-        expr, expected = pair
-
-        pd = pm.ARG.parseString(expr)
-        assert pd[0] == expected
-
-    @pytest.mark.parametrize(
-        'pair',
-        [('1', [1]),
-         ('1,3.14,lero,true,false,null', [1, 3.14, 'lero', True, False, None])])
-    def test_arglist(self, pair):
-        expr, expected = pair
-
-        pd = pm.ARGLIST.parseString(expr)
-        assert list(pd) == expected
+        assert pd[0] == rep
 
     @pytest.mark.parametrize(
         'pair',
         [('a()', []),
-         ('a(1)', [1]),
-         ('a(x,y,z)', ['x', 'y', 'z']),
-         ('a(string:1,string:2)', ['1', '2']),
+         ('a(1)', ['1']),
+         ('a(x,(y,z))', ['x', ('y', 'z')]),
+         ('a(number:1,string:2)', [1, '2']),
          ('a(date:2017-01-01)', [datetime.date(2017, 1, 1)])
          ])
-    def test_simple_call(self, pair):
+    def test_CALL_OPERATOR(self, pair):
         expr, args = pair
 
-        pd = pm.SIMPLE_CALL.parseString(expr)
+        pd = pm.CALL_OPERATOR.parseString(expr)
         assert pd[0] == {'name': 'a', 'args': args}
+
+    def test_nested_CALL_OPERATOR(self):
+        expr = 'a(1,b(2),3)'
+
+        pd = pm.CALL_OPERATOR.parseString(expr)
+        assert pd[0] == {'name': 'a', 'args': ['1', {'name': 'b', 'args': ['2']}, '3']}
 
     @pytest.mark.parametrize(
         'pair',
-        [('a()', []),
-         ('a(1)', [1]),
-         ('a(x,y,z)', ['x', 'y', 'z']),
-         ('a(string:1,number:2)', ['1', 2]),
-         ('a(date:2017-01-01)', [datetime.date(2017, 1, 1)])
-         ])
-    def test_call(self, pair):
-        expr, args = pair
-
-        pd = pm.CALL.parseString(expr)
-        assert pd[0] == {'name': 'a', 'args': args}
-
-    @pytest.mark.parametrize(
-        'pair',
-        [('a=1', ['a', 1]), ('a=xyz', ['a', 'xyz']), ('a=string:1', ['a', '1']),
+        [('a=1', ['a', '1']), ('a=xyz', ['a', 'xyz']), ('a=string:1', ['a', '1']),
          ('a=date:2017-01-01', ['a', datetime.date(2017, 1, 1)])])
     def test_call_eq_expressions(self, pair):
         expr, args = pair
 
-        pd = pm.CALL.parseString(expr)
+        pd = pm.COMPARISON.parseString(expr)
         assert pd[0] == {'name': 'eq', 'args': args}
 
     @pytest.mark.parametrize('op', CMP_OPS)
     @pytest.mark.parametrize(
         'pair',
-        [('a=%s=1', ['a', 1]), ('a=%s=xyz', ['a', 'xyz']), ('a=%s=string:1', ['a', '1']),
+        [('a=%s=1', ['a', '1']), ('a=%s=xyz', ['a', 'xyz']), ('a=%s=string:1', ['a', '1']),
          ('a=%s=date:2017-01-01', ['a', datetime.date(2017, 1, 1)])])
     def test_call_fiql_expression(self, op, pair):
         expr, args = pair
 
-        pd = pm.CALL.parseString(expr % op)
+        pd = pm.COMPARISON.parseString(expr % op)
         assert pd[0] == {'name': op, 'args': args}
 
-    def test_call_nested(self):
-        expr = 'a(1,b(2),3)'
+    @pytest.mark.parametrize(
+        'pair',
+        [('a()', []),
+         ('a(1)', ['1']),
+         ('a(x,(y,z))', ['x', ('y', 'z')]),
+         ('a(number:1,string:2)', [1, '2']),
+         ('a(date:2017-01-01)', [datetime.date(2017, 1, 1)])
+         ])
+    def test_OPERATOR_call(self, pair):
+        expr, args = pair
 
-        pd = pm.CALL.parseString(expr)
-        assert pd[0] == {'name': 'a', 'args': [1, {'name': 'b', 'args': [2]}, 3]}
+        pd = pm.OPERATOR.parseString(expr)
+        assert pd[0] == {'name': 'a', 'args': args}
+
+    @pytest.mark.parametrize(
+        'pair',
+        [('a=1', ['a', '1']), ('a=xyz', ['a', 'xyz']), ('a=string:1', ['a', '1']),
+         ('a=date:2017-01-01', ['a', datetime.date(2017, 1, 1)])])
+    def test_OPERATOR_eq_expressions(self, pair):
+        expr, args = pair
+
+        pd = pm.OPERATOR.parseString(expr)
+        assert pd[0] == {'name': 'eq', 'args': args}
 
     @pytest.mark.parametrize('op', CMP_OPS)
     @pytest.mark.parametrize(
         'pair',
-        [('%s(a,1)', ['a', 1]), ('%s(a,xyz)', ['a', 'xyz']), ('%s(a,string:1)', ['a', '1']),
-         ('%s(a,(1,2,3))', ['a', (1, 2, 3)]),
-         ('%s(a,date:2017-01-01)', ['a', datetime.date(2017, 1, 1)])])
-    def test_call_cmp_functions(self, op, pair):
+        [('a=%s=1', ['a', '1']), ('a=%s=xyz', ['a', 'xyz']), ('a=%s=string:1', ['a', '1']),
+         ('a=%s=date:2017-01-01', ['a', datetime.date(2017, 1, 1)])])
+    def test_OPERATOR_fiql_expression(self, op, pair):
         expr, args = pair
 
-        pd = pm.CALL.parseString(expr % op)
+        pd = pm.OPERATOR.parseString(expr % op)
         assert pd[0] == {'name': op, 'args': args}
 
-    def test_and_operator(self):
+    def test_AND(self):
         expr = 'a(1)&b(2)'
 
-        pd = pm.OPCALL.parseString(expr)
-        assert pd[0] == {'name': 'and', 'args': [{'name': 'a', 'args': [1]},
-                                                 {'name': 'b', 'args': [2]}]}
+        pd = pm.AND.parseString(expr)
+        assert pd[0] == {'name': 'and', 'args': [{'name': 'a', 'args': ['1']},
+                                                 {'name': 'b', 'args': ['2']}]}
 
-    def test_or_operator(self):
+    def test_OR(self):
         expr = 'a(1)|b(2)'
 
-        pd = pm.OPCALL.parseString(expr)
-        assert pd[0] == {'name': 'or', 'args': [{'name': 'a', 'args': [1]},
-                                                 {'name': 'b', 'args': [2]}]}
-
-    def test_call_parenthesis(self):
-        expr = '(a(1))'
-
-        pd = pm.ARG.parseString(expr)
-        assert pd[0] == {'name': 'a', 'args': [1]}
+        pd = pm.OR.parseString(expr)
+        assert pd[0] == {'name': 'or', 'args': [{'name': 'a', 'args': ['1']},
+                                                {'name': 'b', 'args': ['2']}]}
 
 
 class TestParser:
+
+    def test_simple_expr(self):
+        expr = 'eq(a,1)'
+        rep = {'name': 'eq', 'args': ['a', '1']}
+
+        pd = parser.parse(expr)
+        assert pd == rep
+
+    @pytest.mark.parametrize('op', CMP_OPS)
+    @pytest.mark.parametrize(
+        'exre',
+        [('%s(a,1)', ['a', '1']),
+         ('%s(a,xyz)', ['a', 'xyz']),
+         ('%s(a,string:1)', ['a', '1']),
+         ('%s(a,date:2017-01-01)', ['a', datetime.date(2017, 1, 1)])])
+    def test_op_calls(self, op, exre):
+        expr, rep = exre
+
+        pd = parser.parse(expr % op)
+
+        assert pd == {'name': op, 'args': rep}
+
     def test_equality_operator(self):
         p1 = parser.parse('lero=0')
         p2 = parser.parse('eq(lero, 0)')
-
         assert p1 == p2
 
     def test_and_operator_pair(self):
         p1 = parser.parse('eq(a,0)&eq(b,1)')
         p2 = parser.parse('and(eq(a, 0), eq(b, 1))')
 
-        p3 = {'name': 'and', 'args': [{'name': 'eq', 'args': ['a', 0]},
-                                      {'name': 'eq', 'args': ['b', 1]}]}
+        p3 = {'name': 'and', 'args': [{'name': 'eq', 'args': ['a', '0']},
+                                      {'name': 'eq', 'args': ['b', '1']}]}
 
         assert p1 == p2
         assert p1 == p3
 
     def test_and_operator_triple(self):
         p1 = parser.parse('eq(a,0)&eq(b,1)&eq(c,2)')
-        p2 = parser.parse('and(eq(a, 0), eq(b, 1))')
+        p2 = parser.parse('and(eq(a, 0), eq(b, 1), eq(c, 2))')
 
-        p3 = {'name': 'and', 'args': [{'name': 'eq', 'args': ['a', 0]},
-                                      {'name': 'eq', 'args': ['b', 1]},
-                                      {'name': 'eq', 'args': ['c', 2]}]}
+        p3 = {'name': 'and', 'args': [{'name': 'eq', 'args': ['a', '0']},
+                                      {'name': 'eq', 'args': ['b', '1']},
+                                      {'name': 'eq', 'args': ['c', '2']}]}
 
         assert p1 == p2
         assert p1 == p3
 
-
     def test_or_operator(self):
-        p1 = parser.parse('eq(a,0)|eq(b,1)')
-        p2 = parser.parse('or(eq(a, 0), eq(b, 1))')
+        p1 = parser.parse('(f(1)|f(2))')
+        p2 = parser.parse('or(f(1),f(2))')
 
-        p3 = {'name': 'or', 'args': [{'name': 'eq', 'args': ['a', 0]},
-                                     {'name': 'eq', 'args': ['b', 1]}]}
+        p3 = {'name': 'or', 'args': [{'name': 'f', 'args': ['1']},
+                                     {'name': 'f', 'args': ['2']}]}
 
         assert p1 == p2
         assert p1 == p3
@@ -228,24 +243,18 @@ class TestParser:
         assert parser.parse(expr) == parsed
         assert unparser.unparse(parsed, level=1) == expr
 
-    def test_sort_function(self):
-        expr = 'sort(+lero)'
-        parsed = {'name': 'sort', 'args': [('+', 'lero')]}
+    @pytest.mark.parametrize(
+        'exre',
+        [('sort(+lero)', [('+', 'lero')]),
+         ('sort(+foo,-bar)', [('+', 'foo'), ('-', 'bar')]),
+         ('sort(+(foo,bar),-lero)', [('+', ('foo', 'bar')), ('-', 'lero')]),
+         ])
+    def test_sort_function(self, exre):
+        expr, args = exre
 
-        assert parser.parse(expr) == parsed
-        assert unparser.unparse(parsed) == expr
+        pd = parser.parse(expr)
 
-        expr = 'sort(+foo,-bar)'
-        parsed = {'name': 'sort', 'args': [('+', 'foo'), ('-', 'bar')]}
-
-        assert parser.parse(expr) == parsed
-        assert unparser.unparse(parsed) == expr
-
-        expr = 'sort(+(foo,bar),-lero)'
-        parsed = {'name': 'sort', 'args': [('+', ('foo', 'bar')), ('-', 'lero')]}
-
-        assert parser.parse(expr) == parsed
-        assert unparser.unparse(parsed) == expr
+        assert pd == {'name': 'sort', 'args': args}
 
     @pytest.mark.parametrize('func', ['select', 'values'])
     def test_query_functions(self, func):
@@ -273,7 +282,7 @@ class TestParser:
 
     def test_limit_function(self):
         expr = 'limit(10,0)'
-        parsed = {'name': 'limit', 'args': [10, 0]}
+        parsed = {'name': 'limit', 'args': ['10', '0']}
 
         assert parser.parse(expr) == parsed
         assert unparser.unparse(parsed) == expr
@@ -292,21 +301,19 @@ class TestParser:
     def test_equivalent_expressions(self, expr):
         parsed = parser.parse(expr)
 
-        assert parsed == {'name': 'and', 'args': [{'name': 'eq', 'args': ['foo', 3]},
-                                                  {'name': 'lt', 'args': ['price', 10]}]}
+        assert parsed == {'name': 'and', 'args': [{'name': 'eq', 'args': ['foo', '3']},
+                                                  {'name': 'lt', 'args': ['price', '10']}]}
 
     def test_toplevel_and(self):
         parsed = parser.parse('eq(a, 1),eq(b, 2),eq(c, 3)')
 
-        assert parsed == {'name': 'and', 'args': [{'name': 'eq', 'args': ['a', 1]},
-                                                  {'name': 'eq', 'args': ['b', 2]},
-                                                  {'name': 'eq', 'args': ['c', 3]},
+        assert parsed == {'name': 'and', 'args': [{'name': 'eq', 'args': ['a', '1']},
+                                                  {'name': 'eq', 'args': ['b', '2']},
+                                                  {'name': 'eq', 'args': ['c', '3']},
                                                   ]}
 
     def test_parenthesis(self):
         parsed = parser.parse('(state=Florida|state=Alabama)&gender=female')
-
-        import pdb; pdb.set_trace()
 
         assert parsed == {'name': 'and',
                           'args': [{'name': 'or',
@@ -315,3 +322,65 @@ class TestParser:
                                              ],
                                     },
                                    {'name': 'eq', 'args': ['gender', 'female']}]}
+
+
+class TestRFCExamples:
+
+    def test_abstract_example(self):
+        expr = 'category=dates&sort(+price)'
+        rep = {'name': 'and', 'args': [{'name': 'eq', 'args': ['category', 'dates']},
+                                       {'name': 'sort', 'args': [('+', 'price')]}]}
+
+        pd = parser.parse(expr)
+        assert pd == rep
+
+    def test_arrays_example(self):
+        expr = 'in(category,(toy,food))'
+        rep = {'name': 'in', 'args': ['category', ('toy', 'food')]}
+
+        pd = parser.parse(expr)
+        assert pd == rep
+
+    def test_nested_operators_example(self):
+        expr = 'or(eq(category,toy),eq(category,food))'
+        rep = {'name': 'or', 'args': [{'name': 'eq', 'args': ['category', 'toy']},
+                                      {'name': 'eq', 'args': ['category', 'food']}]}
+
+        pd = parser.parse(expr)
+        assert pd == rep
+
+    @pytest.mark.parametrize(
+        'exre',
+        [('sort(+foo)', [('+', 'foo')]),
+         ('sort(+price,-rating)', [('+', 'price'), ('-', 'rating')]),
+         ])
+    def test_sort_examples(self, exre):
+        expr, args = exre
+
+        pd = parser.parse(expr)
+        assert pd == {'name': 'sort', 'args': args}
+
+    def test_aggregate_example(self):
+        expr = 'aggregate(departmentId,sum(sales))'
+        rep = {'name': 'aggregate', 'args': ['departmentId', {'name': 'sum', 'args': ['sales']}]}
+
+        pd = parser.parse(expr)
+        assert pd == rep
+
+    def test_comparison_example(self):
+        expr = 'foo=3&(bar=text|bar=string)'
+        rep = {'name': 'and', 'args': [{'name': 'eq', 'args': ['foo', '3']},
+                                       {'name': 'or', 'args': [
+                                           {'name': 'eq', 'args': ['bar', 'text']},
+                                           {'name': 'eq', 'args': ['bar', 'string']},
+                                           ]}]}
+
+        pd = parser.parse(expr)
+        assert pd == rep
+
+    def test_typed_value_example(self):
+        expr = 'foo=number:4'
+        rep = {'name': 'eq', 'args': ['foo', 4]}
+
+        pd = parser.parse(expr)
+        assert pd == rep
