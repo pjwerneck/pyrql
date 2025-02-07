e088074ba3e213c3fa20db51f7d5b29538e4f80b
Pedro Werneck <pjwerneck@gmail.com>
2025-02-07 16:29:17 -0300

    Bump version: 0.7.8 → 0.7.9

--- pyrql/__init__.py
+++ pyrql/__init__.py
@@ -6,7 +6,7 @@ from pyrql.query import Query
 from pyrql.unparser import Unparser
 
 __title__ = "pyrql"
-__version__ = "0.7.8"
+__version__ = "0.7.9"
 __author__ = "Pedro Werneck"
 __license__ = "MIT"
 
